82e80178ec18a711d40336c1cc29ed685e1e3c5e
Lt-Henry <quiqueiii@gmail.com>
2014-06-22 13:58:08 +0200

    small and broken SDL startup code

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,13 +1,14 @@
 
 
 #include "Game.hpp"
+#include <SDL2/SDL.h>
 
 #include <iostream>
 
 using namespace std;
 using namespace com::toxiclabs::galaxy;
 
-Game * Game::factory = NULL;
+Game * Game::factory = nullptr;
 
 
 Game::Game()
@@ -23,7 +24,7 @@ Game::~Game()
 
 Game * Game::GetGame()
 {
-	if(Game::factory==NULL)
+	if(Game::factory==nullptr)
 		Game::factory = new Game();
 		
 		return Game::factory;
@@ -31,7 +32,7 @@ Game * Game::GetGame()
 
 void Game::Free()
 {
-	if(Game::factory!=NULL)
+	if(Game::factory!=nullptr)
 	{
 		delete Game::factory;
 	}
@@ -40,4 +41,23 @@ void Game::Free()
 void Game::Run()
 {
 	cout<<"Game main loop"<<endl;
+	
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+	{
+		cerr<<"Failed to initialize SDl"<<endl;
+	}
+	
+	SDL_Window *win = SDL_CreateWindow("Galaxy Delivery", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+	
+	if(win==nullptr)
+	{
+		
+	}
+	
+	SDL_Renderer *ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	
+	if(ren==nullptr)
+	{
+		
+	}
 }
